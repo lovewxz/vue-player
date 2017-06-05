203567930da53a2b796f18e6f7100b9f124cc6a1
网络营销-技术 <wlyx@wangluoyingxiao-jishudeMac-mini.local>
2017-06-05 18:00:18 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <p>hello</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style lang="scss">
+@import "./common/scss/variable";
+
+#app {
+  color: $color-theme;
+}
+</style>
